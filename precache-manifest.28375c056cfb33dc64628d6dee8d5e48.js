b43fe08e7ec6d0ff01bf74a0ce5929c3b7c831fd
RajshekharMuchandi <rsmuchandi@gmail.com>
2020-07-13 20:46:32 +0530

    Updates

---
+++ precache-manifest.28375c056cfb33dc64628d6dee8d5e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65b6181bd952e0f83cff8df7ed5f4d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f667c32b8ac82980bf",
+    "url": "/static/css/main.f09ba729.chunk.css"
+  },
+  {
+    "revision": "0ab39b5678f470e1f258",
+    "url": "/static/js/2.26de290a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26de290a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f667c32b8ac82980bf",
+    "url": "/static/js/main.37840a23.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
